1b6950c2c5fcb5523174893cf4eb08a3f169fded
avramenkomy <mihail.avramenko@ocrv.ru>
2022-04-12 10:48:57 +0300

    WIP: connect fron and back

--- altika_backend/altika_backend/settings.py
+++ altika_backend/altika_backend/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'altika_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(REACT_APP_PATH, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,7 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(REACT_APP_PATH, 'build', 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
