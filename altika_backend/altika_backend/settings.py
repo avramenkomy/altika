667013ff3af5c20848d40982fb07215c8c937501
avramenkomy <mihail.avramenko@ocrv.ru>
2022-04-13 18:13:36 +0300

    WIP: change settings for deploy to host reg.ru

--- altika_backend/altika_backend/settings.py
+++ altika_backend/altika_backend/settings.py
@@ -28,9 +28,10 @@ REACT_APP_PATH = os.path.join(BASE_DIR.parent, 'altika_frontend') # корень
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
@@ -44,11 +45,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # library
     'rest_framework',
+    'corsheaders',
     # my_apps
     'send_email',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -82,10 +85,20 @@ WSGI_APPLICATION = 'altika_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'u1651207_default',
+        'USER': 'u1651207_default',
+        'PASSWORD': 'tsOU7eQm4T53KxyG',
+        'HOST': 'localhost',
     }
 }
 
@@ -125,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT='static/'
 STATICFILES_DIRS = [os.path.join(REACT_APP_PATH, 'build', 'static')]
 
 # Default primary key field type
