0a0e38bde6202c950a063f79261780d888d0e167
avramenkomy <mihail.avramenko@ocrv.ru>
2022-04-18 21:17:26 +0300

    WIP: code decomposition

--- altika_frontend/src/App.js
+++ altika_frontend/src/App.js
@@ -1,6 +1,4 @@
-import React, { useState } from 'react';
-import { ThemeProvider, createTheme } from '@material-ui/core/styles';
-
+import React from 'react';
 import AppHeader from './components/appHeader/AppHeader';
 import About from './components/about/About';
 import Intro from './components/intro/Intro';
@@ -15,60 +13,18 @@ import Notes from './components/notes/Notes';
 
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  const theme = createTheme({
-    palette: {
-      type: darkMode ? 'dark' : 'light',
-      primary: {
-        light: '#508BB6',
-        main: '#256EA4',
-        dark: '#006EAA',
-        contrastText: '#F0F0F1'
-      },
-      secondary: {
-        light: '#FFF',
-        main: '#E0E1E2',
-        dark: '#F0F0F1',
-        contrastText: '#000'
-      },
-    },
-    typography: {
-      h2: {
-        '@media (max-width: 501px)': {
-          fontSize: '40px',
-        },
-        '@media (max-width: 401px)': {
-          fontSize: '30px',
-        },
-        '@media (max-width: 301px)': {
-          fontSize: '20px',
-        },
-      },
-      h6: {
-        fontSize: '1rem',
-        '@media (min-width: 1280px)': {
-          fontSize: '1.25rem',
-        },
-        '@media (min-width: 1920px)': {
-          fontSize: '1.5rem',
-        }
-      }
-    }
-  });
-
   return (
-    <ThemeProvider theme={theme}>
+    <>
       <div id="header" />
-      <AppHeader darkTheme={darkMode} onChangeTheme={setDarkMode} />
+      <AppHeader />
       <main>
         <Intro />
         <div id="about" />
-        <About darkTheme={darkMode} />
+        <About />
         <div id="expertise_types" />
         <ExpertiseTypes />
         <div id="services" />
-        <Services darkMode={darkMode} />
+        <Services />
         <StepByWork />
         <ReasonsForExpertise />
         <License />
@@ -79,7 +35,7 @@ function App() {
         <div id='contacts' />
         <FooterComponent />
       </footer>
-    </ThemeProvider>
+    </>
   );
 }
 
--- altika_frontend/src/index.js
+++ altika_frontend/src/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import { SnackbarProvider } from 'notistack';
+import { ThemeProvider } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 
+import { SnackbarUtilsConfigurator } from './services/elements/OutsideSnackbar';
+
+import App from './App';
 import './styles/style.css';
+import theme from './services/theme';
+
+const notistackRef = React.createRef();
+const onClickDismiss = key => () => { 
+  notistackRef.current.closeSnackbar(key);
+}
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <SnackbarProvider
+      ref={notistackRef}
+      maxSnack={3}
+      anchorOrigin={{ vertical: "bottom", horizontal: "right"}}
+      autoHideDuration={8000}
+      action={(key) => (
+        <IconButton size="small" aria-label="close" color="inherit" onClick={onClickDismiss(key)}>
+          <CloseIcon fontSize="small" />
+        </IconButton>)
+      }
+    >
+      <SnackbarUtilsConfigurator />
+      <App />
+    </SnackbarProvider>
+  </ThemeProvider>
+);
